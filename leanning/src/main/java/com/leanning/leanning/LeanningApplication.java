3b0da239b599dacb1928f44eda0c2a58d3a19ac5
Thanet Wongduean <119989079+Thanet04@users.noreply.github.com>
2024-10-08 00:41:08 +0700

    ลง java spring boot

---
+++ leanning/src/main/java/com/leanning/leanning/LeanningApplication.java
@@ -0,0 +1,13 @@
+package com.leanning.leanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanningApplication.class, args);
+	}
+
+}
---
+++ leanning/src/test/java/com/leanning/leanning/LeanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leanning.leanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
