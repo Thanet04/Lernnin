aebc9b785dae76f9208ca607d2a56f15ba1523ec
Thanet Wongduean <119989079+Thanet04@users.noreply.github.com>
2024-10-13 17:22:00 +0700

    done โครง html

--- Tableangular/src/app/app.routes.ts
+++ Tableangular/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 
 // Export ตัวแปร routes
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'create', component: CreateComponent },
 ];
 
 @NgModule({
---
+++ Tableangular/src/app/create/create.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.scss'
+})
+export class CreateComponent {
+
+}
